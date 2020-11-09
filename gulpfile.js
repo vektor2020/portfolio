0d96326e33d25fdd2a0f1466e6d5996fac1c0be6
vektor2020 <lexaro2011@gmail.com>
2020-11-09 22:34:16 +0200

    gulpfiles

--- gulpfile.js
+++ gulpfile.js
@@ -6,11 +6,9 @@ var sass = require('gulp-sass');
 sass.compiler = require('node-sass');
  
 gulp.task('sass', function () {
-  return gulp.src('./sass/**/*.scss')
-    .pipe(sass().on('error', sass.logError))
+  return gulp.src('./sass/style.scss')
+    .pipe(sass.sync().on('error', sass.logError))
     .pipe(gulp.dest('./css'));
 });
  
-gulp.task('sass:watch', function () {
-  gulp.watch('./sass/**/*.scss', ['sass']);
-});
+gulp.watch('./sass/**/*.scss', gulp.parallel('sass'));
